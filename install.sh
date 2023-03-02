9c17066bc55f946aa6c6f3b2a4fb037d035a03cf
Andre Macedo <contato@andremacedo.dev>
2023-03-02 01:25:17 -0300

    feat: improvements on install script

--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-mkdir ~/.config
-
-# ============= NEOVIM
-
-rm -rf ~/.config/nvim
-ln -s $HOME/dev/dotfiles/nvim/ ~/.config/nvim
-
-# packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# packer install plugins
-nvim +PackerSync +qall
+CONFIGDIR=~/.config
+[ ! -d "$CONFIGDIR" ] && mkdir $CONFIGDIR
 
 
 # ============= POLYBAR
@@ -71,6 +60,21 @@ then
 	echo "makepkg -i"
 	echo ""
 fi
+
+
+# ============= NEOVIM
+
+rm -rf ~/.config/nvim
+ln -s $HOME/dev/dotfiles/nvim/ ~/.config/nvim
+
+# packer
+PACKERDIR=~/.local/share/nvim/site/pack/packer/start/packer.nvim
+if [ ! -d "$PACKERDIR" ]; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim $PACKERDIR
+fi
+
+# packer install plugins
+# nvim +PackerSync
 
 
 # ============ done
