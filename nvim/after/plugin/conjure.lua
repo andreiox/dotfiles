22e518e860d04e8a7c55ef81a1ea3cd071456d16
Andre Macedo <andre.macedo@nubank.com.br>
2023-03-15 12:47:02 -0300

    feat: add conjure test clojure ns

--- nvim/after/plugin/conjure.lua
+++ nvim/after/plugin/conjure.lua
@@ -9,3 +9,6 @@ vim.keymap.set("n", "<leader>eb", [[:ConjureEvalBuf<CR>]])
 -- repl output
 vim.keymap.set("n", "<leader>rv", [[:ConjureLogVSplit<CR>]])
 vim.keymap.set("n", "<leader>rc", [[:ConjureLogResetSoft<CR>]])
+
+-- tests
+vim.keymap.set("n", "<leader>tn", [[:ConjureCljRunCurrentNsTests<CR>]])
