2f845e89f4373ddc2e895f1f31b42df34c4b301e
Andre Macedo <andre.macedo@nubank.com.br>
2023-04-24 14:30:34 -0300

    fix: gruvbox italic configuration

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -1,8 +1,22 @@
 local gruvbox = require('gruvbox')
 
 gruvbox.setup({
-  italic = false,
-  bold = false
+  contrast = "hard",
+  palette_overrides = {dark0_hard = "#0E1018"},
+  italic = {
+     strings = false,
+     operators = false,
+     comments = true
+  },
+  bold = true,
+  overrides = {
+    Comment = {fg = "#81878f", italic = true},
+    -- new git colors
+    DiffAdd = { bold = true, reverse = false, fg = "", bg = "#2a4333"},
+    DiffChange = { bold = true, reverse = false, fg = "", bg = "#333841" },
+    DiffDelete = { bold = true, reverse = false, fg = "#442d30", bg = "#442d30" },
+    DiffText = { bold = true, reverse = false, fg = "", bg = "#213352" },
+  }
 })
 
 vim.o.background = "dark"
