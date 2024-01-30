0ccf8bae7164a4f9f079c845235ce30be36a44ed
André Macedo <dee.xd@hotmail.com>
2024-01-29 22:02:54 -0300

    feat: fix treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "javascript", "typescript", "clojure" },
+  ensure_installed = { "c", "lua", "vim", "javascript", "typescript", "clojure" },
 
   sync_install = false,
   auto_install = true,
