016426f787c5ea62000c559e4a08007ddd404eb8
André Macedo <dee.xd@hotmail.com>
2025-11-23 11:52:15 -0300

    fix: polybar tray displaying on fixed monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+tray_output=HDMI-A-0
+
 # Launch Polybar, using default config location ~/.config/polybar/config
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-  MONITOR=$m polybar --reload mybar &
+  # display tray only on tray_output
+  export MONITOR=$m
+  export TRAY_POSITION=none
+  if [[ $m == $tray_output ]]; then
+    TRAY_POSITION=right
+  fi
+
+  polybar --reload mybar &
 done
 
 echo "Polybar launched..."
